bb6cc94ed5176568da3d3ab5f5870dd56508bd98
gabrielcilico <gabrielcilico@gmail.com>
2020-06-16 23:37:47 -0300

    Adicionando card de exemplo

---
+++ src/components/QuestionCard.vue
@@ -0,0 +1,16 @@
+<template>
+    <div>
+        <h1>Titulo</h1>
+        <p>pergunta</p>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'QuestionCard'
+}
+</script>
+
+<style>
+
+</style>
