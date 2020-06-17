d5683e089925cafd876c5baab7ca42026f65ac5a
gabrielcilico <gabrielcilico@gmail.com>
2020-06-16 23:24:11 -0300

    Ajustes iniciais

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,26 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
+    <Header />
+    <router-view />
   </div>
 </template>
 
+<script>
+import Header from '@/components/Header.vue';
+
+export default {
+  name: 'Home',
+  components: {
+    Header,
+  },
+};
+</script>
+
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-
-#nav {
-  padding: 30px;
-}
-
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-#nav a.router-link-exact-active {
-  color: #42b983;
+*,
+body {
+  padding: 0;
+  margin: 0;
+  font-family: 'Press Start 2P', cursive;
 }
 </style>
---
+++ src/components/Header.vue
@@ -0,0 +1,21 @@
+<template>
+  <header>
+    <h1 class="nes-text is-primary">Quizz</h1>
+    <router-link to="/about">Sobre</router-link>
+  </header>
+</template>
+
+<script>
+export default {
+  name: 'Header',
+};
+</script>
+
+<style>
+header {
+  background-color: red;
+  display: flex;
+  justify-content: space-between;
+  padding: 15px 20px;
+}
+</style>
